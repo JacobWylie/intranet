8c7624d083ca1e6c67ca85e3cc5abd71601a3362
JacobWylie <jacobwylie@gmail.com>
2017-02-23 17:03:52 +0100

    Cleaned up variable names

--- js/rooms.js
+++ js/rooms.js
@@ -2,18 +2,18 @@ var meetingRooms = new XMLHttpRequest();
 meetingRooms.onreadystatechange = function () {
 	if(meetingRooms.readyState === 4 && meetingRooms.status === 200) {
 		var rooms = JSON.parse(meetingRooms.responseText);
-		var roomsHTML = '<ul class="rooms">';
+		var statusHTML = '<ul class="rooms">';
 		for (var i=0; i<rooms.length; i += 1) {
 			if (rooms[i].available === true) {
-				roomsHTML += '<li class="empty">';
+				statusHTML += '<li class="empty">';
 			} else {
-				roomsHTML += '<li class="full">';
+				statusHTML += '<li class="full">';
 			}
-			roomsHTML += rooms[i].room;
-			roomsHTML += '</li>';
+			statusHTML += rooms[i].room;
+			statusHTML += '</li>';
 		}
-		roomsHTML += '</ul>';
-		document.getElementById('roomList').innerHTML = roomsHTML;
+		statusHTML += '</ul>';
+		document.getElementById('roomList').innerHTML = statusHTML;
 	}
 };
 meetingRooms.open('GET', '../data/rooms.json');
